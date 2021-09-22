860e4c4abbc89a34aa812c325cbe59e288fac48f
Thurston Alswager <thurston_alswager@yahoo.com>
2021-09-21 19:20:11 -0500

    Added endpoints

--- server.js
+++ server.js
@@ -5,8 +5,27 @@ const app = express();
 
 connectDatabase();
 
+//Configure Middleware
+app.use(express.join({ extended: false }));
+
+//Endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
+//Endpoints
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+//Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
