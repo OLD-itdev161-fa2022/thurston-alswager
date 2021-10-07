2f8bacead6726477536c5b31b50e8b024b789c2c
Thurston Alswager <thurston_alswager@yahoo.com>
2021-10-06 20:08:02 -0500

    Completed Activity 4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
+import cors from 'cors';
 
 const app = express();
 
@@ -8,6 +9,11 @@ connectDatabase();
 
 //Configure Middleware
 app.use(express.json({ extended: false }));
+app.use(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+);
 
 //Endpoints
 /**
@@ -46,4 +52,5 @@ app.post(
 );
 
 //Connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
